222e3bc2aa6ae1cf45c34e7732e561fceedd8fb6
ELB HAKIM <hakim@MacBook-Air-de-ELB.local>
2026-03-04 01:02:41 +0100

    Add Swagger documentation

--- src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
+++ src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
@@ -1,8 +1,17 @@
 package com.dauphine.blogger;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Blogger box backend",
+				description = "Blogger box endpoints and apis",
+				contact = @Contact(name = "Hakim", email = "hakim70a@hotmail.com"),
+				version = "1.0.0"
+		)
+)
 @SpringBootApplication
 public class BloggerBoxBackendApplication {
 
